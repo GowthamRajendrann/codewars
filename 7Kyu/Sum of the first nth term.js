1b6e495f5866f240d91d961f5891e7e78e3ec890
GowthamRajendrann <grr1696@gmail.com>
2022-07-20 22:26:53 +0530

    add sum series problem

---
+++ 7Kyu/Sum of the first nth term.js
@@ -0,0 +1,42 @@
+// Sum of the first nth term of Series
+// Task:
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+
+// If the given value is 0 then it should return 0.00
+
+// You will only be given Natural Numbers as arguments.
+
+// Examples:(Input --> Output)
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+/* My Solution*/
+function SeriesSum(n) {
+  let sum = 0;
+  let j = 1;
+
+  //Not applicable
+  for (let i = 1; i <= n; i += 1) {
+    a = +(1 / j);
+    j += 3;
+    sum += a;
+  }
+  if (n === 0) {
+    return n.toFixed(2);
+  }
+  return sum.toFixed(2);
+}
+
+/* Best Solution*/
+function SeriesSum(n) {
+  for (var s = 0, i = 0; i < n; i++) {
+    s += 1 / (1 + i * 3);
+  }
+
+  return s.toFixed(2);
+}
