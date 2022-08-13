c809fcacdf0fe500b44b392d0a49907583e2cfbe
GowthamRajendrann <grr1696@gmail.com>
2022-08-13 19:16:17 +0530

    add Direction Reduction

---
+++ 5kyu/DirectionReduction.js
@@ -0,0 +1,58 @@
+// Direction Reduction
+// Once upon a time, on a way through the old wild mountainous west,…
+// … a man was given directions to go from one point to another. The directions were "NORTH", "SOUTH", "WEST", "EAST". Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too.
+
+// Going to one direction and coming back the opposite direction right away is a needless effort. Since this is the wild west, with dreadful weather and not much water, it's important to save yourself some energy, otherwise you might die of thirst!
+
+// How I crossed a mountainous desert the smart way.
+// The directions given to the man are, for example, the following (depending on the language):
+
+// ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"].
+// or
+// { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
+// or
+// [North, South, South, East, West, North, West]
+// You can immediately see that going "NORTH" and immediately "SOUTH" is not reasonable, better stay to the same place! So the task is to give to the man a simplified version of the plan. A better plan in this case is simply:
+
+// ["WEST"]
+// or
+// { "WEST" }
+// or
+// [West]
+// Other examples:
+// In ["NORTH", "SOUTH", "EAST", "WEST"], the direction "NORTH" + "SOUTH" is going north and coming back right away.
+
+// The path becomes ["EAST", "WEST"], now "EAST" and "WEST" annihilate each other, therefore, the final result is [] (nil in Clojure).
+
+//My Solution
+function dirReduc(arr) {
+  arr.forEach(dirDetec);
+  return arr;
+}
+function dirDetec(item, index, arr) {
+  if (
+    (item === "NORTH" && arr[index + 1] === "SOUTH") ||
+    (item === "SOUTH" && arr[index + 1] === "NORTH") ||
+    (item === "EAST" && arr[index + 1] === "WEST") ||
+    (item === "WEST" && arr[index + 1] === "EAST")
+  ) {
+    arr.splice(index, 2);
+    return dirReduc(arr);
+  }
+  return arr;
+}
+
+//Best Solution
+function dirReduc(plan) {
+  var opposite = {
+    NORTH: "SOUTH",
+    EAST: "WEST",
+    SOUTH: "NORTH",
+    WEST: "EAST",
+  };
+  return plan.reduce(function (dirs, dir) {
+    if (dirs[dirs.length - 1] === opposite[dir]) dirs.pop();
+    else dirs.push(dir);
+    return dirs;
+  }, []);
+}
