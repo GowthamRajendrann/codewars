e4bae9b5c63d27b32bd621b1a69d16d3f4f598eb
GowthamRajendrann <grr1696@gmail.com>
2022-09-14 21:43:22 +0530

    add make deadSwim Challenge

---
+++ 6kyu/Deadfish.js
@@ -0,0 +1,34 @@
+// Write a simple parser that will parse and run Deadfish.
+
+// Deadfish has 4 commands, each 1 character long:
+
+// i increments the value (initially 0)
+// d decrements the value
+// s squares the value
+// o outputs the value into the return array
+// Invalid characters should be ignored.
+
+// parse("iiisdoso") => [ 8, 64 ]
+
+// Return the output array, and ignore all non-op characters
+
+function parse(data) {
+  //initialize a value
+  let value = 0;
+  //split array for o
+  let data1 = data.split("o").map((item) => {
+    item.split("").forEach((i) => {
+      if (i === "i") {
+        value += 1;
+      }
+      if (i === "d") {
+        value -= 1;
+      }
+      if (i === "s") {
+        value = value * value;
+      }
+    });
+    return value;
+  });
+  return data1.slice(0, data.split("o").length - 1);
+}
